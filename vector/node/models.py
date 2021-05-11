987b05ee417ae34a08f4e1d8a942f288836fa97f
zaleksandrne <zaleksandrne@gmail.com>
2021-05-11 12:52:42 +0000

    length operation

--- vector/node/models.py
+++ vector/node/models.py
@@ -1,24 +1,30 @@
+import math
+from itertools import zip_longest
+
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from itertools import zip_longest
 from django.db.models.signals import m2m_changed, post_save, pre_delete
 
 
 class Vector(models.Model):
-    array = ArrayField(models.IntegerField())
+    array = ArrayField(models.DecimalField(max_digits=5, decimal_places=2))
 
     def __str__(self):
         return f'id:{self.id}, arr:{self.array}'
 
 
 class Operation(models.Model):
-    CHOICES = (('add', 'addition'), ('mult', 'multiplication'))
+    CHOICES = (('add', 'addition'),
+               ('mult', 'multiplication'),
+               ('length', 'length'))
     type = models.CharField(max_length=100, choices=CHOICES)
     vectors = models.ManyToManyField(
         Vector,
         verbose_name='Vector'
         )
-    array = ArrayField(models.IntegerField(), blank=True, null=True)
+    array = ArrayField(models.DecimalField(max_digits=5, decimal_places=2),
+                                           blank=True,
+                                           null=True)
     new_vector = models.OneToOneField(Vector,
                                       on_delete=models.CASCADE,
                                       related_name='vector',
@@ -43,17 +49,22 @@ def vectors_changed(sender, instance, action, pk_set, **kwargs):
             elif instance.type == 'mult':
                 arr = [x * y for x, y in zip_longest(
                     instance.array, vector.array, fillvalue=1)]
+            elif instance.type == 'length':
+                p = math.sqrt(sum([i * i for i in vector.array]))
+                arr = [n for n in instance.array + [p]]
             instance.array = arr
         try:
             instance.new_vector.array = arr
+            instance.new_vector.save()
         except AttributeError:
             vector = Vector.objects.create(array=instance.array)
             instance.new_vector = vector
         instance.save()
-        for item in pk_set:
-            vector = Vector.objects.get(id=item)
-            vector.array += (len(instance.array) - len(vector.array)) * [0]
-            vector.save()
+        if instance.type != 'length':
+            for item in pk_set:
+                vector = Vector.objects.get(id=item)
+                vector.array += (len(instance.array) - len(vector.array)) * [0]
+                vector.save()
 
 
 def save_operation(sender, instance, **kwargs):
@@ -61,6 +72,16 @@ def save_operation(sender, instance, **kwargs):
     for operation in o:
         operation.array = []
         operation.new_vector.array = []
+        max_l = 0
+        if operation.type != 'length':
+            for vector in operation.vectors.all():
+                if len(vector.array) > max_l:
+                    max_l = len(vector.array)
+            for vector in operation.vectors.all():
+                vector.array += (max_l - len(vector.array)) * [0]
+                post_save.disconnect(save_operation, sender=Vector)
+                vector.save()
+                post_save.connect(save_operation, sender=Vector)
         for vector in operation.vectors.all():
             if operation.type == 'add':
                 arr = [x+y for x, y in zip_longest(
@@ -68,15 +89,13 @@ def save_operation(sender, instance, **kwargs):
             elif operation.type == 'mult':
                 arr = [x * y for x, y in zip_longest(
                     operation.array, vector.array, fillvalue=1)]
+            elif operation.type == 'length':
+                p = math.sqrt(sum([i * i for i in vector.array]))
+                arr = [n for n in operation.array + [p]]
             operation.array = arr
         operation.save()
         operation.new_vector.array = arr
         operation.new_vector.save()
-        for vector in operation.vectors.all():
-            vector.array += (len(operation.array) - len(vector.array)) * [0]
-            post_save.disconnect(save_operation, sender=Vector)
-            vector.save()
-            post_save.connect(save_operation, sender=Vector)
 
 
 def vector_delete_pre(sender, instance, **kwargs):
@@ -94,6 +113,9 @@ def vector_delete_pre(sender, instance, **kwargs):
                 elif operation.type == 'mult':
                     arr = [x * y for x, y in zip_longest(
                         operation.array, vector.array, fillvalue=1)]
+                elif operation.type == 'length':
+                    p = math.sqrt(sum([i * i for i in vector.array]))
+                    arr = [n for n in operation.array + [p]]
                 operation.array = arr
             operation.save()
             operation.new_vector.array = arr
@@ -103,3 +125,4 @@ def vector_delete_pre(sender, instance, **kwargs):
 m2m_changed.connect(vectors_changed, sender=Operation.vectors.through)
 post_save.connect(save_operation, sender=Vector)
 pre_delete.connect(vector_delete_pre, sender=Vector)
+
